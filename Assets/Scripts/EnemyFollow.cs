f4f40f4ab9d815a6607226e454144d4fc1b6d866
JDelB95 <jdelb35@gmail.com>
2021-01-30 14:39:58 -0500

    Basic Enemy AI Added JDel

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+    }
+}
