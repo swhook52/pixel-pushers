ae0a48d1a8114e80d58124e106220b1110558af1
JDelB95 <jdelb35@gmail.com>
2021-01-30 01:43:47 -0500

    Dropping Items Added JDel
    Player can now drop items from their inventory and the item will appear in the game scene.

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    private PlayerInventory inventory;
+    public int i;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
+    }
+
+    private void Update()
+    {
+        if(transform.childCount <= 0)
+        {
+            inventory.isFull[i] = false;
+        }
+    }
+
+    public void DropItem()
+    {
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<SpawnItem>().SpawnDroppedItem();
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+}
