f2dbf6c7fbcf0fc0ee3344d360d579baa60e5723
JDelB95 <jdelb35@gmail.com>
2021-01-30 01:09:49 -0500

    Inventory and Pickup JDel
    Added an inventory system and the ability to pickup items and add them to the player's inventory.

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private PlayerInventory inventory;
+    public GameObject itemButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if(inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
