37714e972bd5c6628c6d25949e2fd84e8f821892
JDelB95 <jdelb35@gmail.com>
2021-01-30 02:05:12 -0500

    Inventory Version 1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,104 @@
+<<<<<<< HEAD
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if(change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+}
+=======
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+ 
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private float movementX;
+    private float movementY;
+    public float speed = 1;
+    public float maxVelocity = 20f;
+    private Player1Controls controls;
+    private Vector2 lookDirection;
+
+    void Awake()
+    {
+        controls = new Player1Controls();
+        controls.Player.Look.performed += Aim;
+    }
+
+    private void Aim(InputAction.CallbackContext context)
+    {
+        lookDirection = context.ReadValue<Vector2>();
+        Debug.Log(lookDirection);
+        Vector2 direction = Camera.main.ScreenToWorldPoint(lookDirection) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+
+        var rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
+        transform.rotation = rotation;
+        //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 1f);
+        //Quaternion rotation = Quaternion.LookRotation(lookDirection);
+        //Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //transform.rotation = rotation;
+    }
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+ 
+    private void OnEnable()
+    {
+        controls.Player.Enable();
+    }
+    private void OnDisable()
+    {    
+        controls.Player.Disable();
+    }
+
+   private void OnMove(InputValue movementValue)
+   {
+       Vector2 movementVector = movementValue.Get<Vector2>();
+       movementX = movementVector.x;
+       movementY = movementVector.y;
+   }
+  
+   void FixedUpdate()
+   {
+       Vector3 movement = new Vector2(movementX, movementY);
+       if (rigidBody.velocity.sqrMagnitude < maxVelocity)
+       {
+           rigidBody.AddForce(movement * speed);
+       }
+   }
+}
+>>>>>>> 7c2300658c4bd20d0d71e846adb9449970cba23b
