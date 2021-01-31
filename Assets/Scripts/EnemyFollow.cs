7c642246ca1d9538d5b85ed5478f8addd771d6cb
JSchlang <john.a.schlanger@gmail.com>
2021-01-31 01:45:36 -0500

    Added zombie animation

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -3,23 +3,28 @@ using UnityEngine;
 public class EnemyFollow : MonoBehaviour
 {
     public float speed = 0.2f;
+    public float dirX;
     public float stoppingDistance;
     public Transform target;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         if (target == null)
         {
+            anim.SetBool("isWalking", false);
             return;
         }
 
         if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
         {
+            anim.SetBool("isWalking", true);
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
     }
@@ -29,7 +34,7 @@ public class EnemyFollow : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             target = collision.transform;
-        }
+        }        
     }
 
     private void OnTriggerExit2D(Collider2D collision)
